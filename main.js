400a739aac8817817394f42eb34f452140c57e85
Kl1menko <klimenko.vitaliy2016@icloud.com>
2022-08-20 21:05:13 +0300

    new faq

--- main.js
+++ main.js
@@ -28,3 +28,35 @@ function closeOnClick() {
     hamb.classList.remove("active");
     body.classList.remove("noscroll");
 }
+
+//FAQ
+
+$(document).ready(function () {
+    $(".set > a").on("click", function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass("active");
+            $(this).siblings('.content').slideUp(200);
+            $(".set > a span").removeClass("minus").addClass("plus");
+        } else {
+            $(".set > a span").removeClass("minus").addClass("plus");
+            $(this).find("span").removeClass("plus").addClass("minus");
+            $(".set > a").removeClass("active");
+            $(this).addClass("active");
+            $('.content').slideUp(200);
+            $(this).siblings('.content').slideDown(200);
+        }
+    });
+});
+
+
+var parentTitleAcc = document.querySelector('.sn-accordion-container');
+var titleAcc = document.querySelector('.sn-accordion-container a');
+
+titleAcc.onclick = function (e) {
+    e.preventDefault();
+    var target = e.target;
+
+    if (target.titleAcc != 'a') return;
+
+    highlight(target);
+};
